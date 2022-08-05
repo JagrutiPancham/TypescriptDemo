007dd7888c142d8604871b25756a5d2ead783732
Jagruti Pancham <jagruti.pancham@corp.ola.com>
2022-08-05 10:38:20 +0530

    added files showing workflow of ts

---
+++ .vscode/app.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.warn("test function working");
+    };
+    return App;
+}());
+var a = new App();
+a.test();
---
+++ .vscode/app.ts
@@ -0,0 +1,11 @@
+
+// let a = "hello";
+// console.warn(a)
+export{}
+class App{
+test(){
+    console.warn("test function working")
+}
+}
+let a = new App();
+a.test()
